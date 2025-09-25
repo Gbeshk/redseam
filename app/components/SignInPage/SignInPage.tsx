fd1e8769602c8d1007e5eebc46c3174ae62a075f
Giorgi Beshkenadze <104314297+Gbeshk@users.noreply.github.com>
2025-09-25 20:11:41 +0400

    fix(auth): add server-side cookie check and redirect in sign-in page

--- app/components/SignInPage/SignInPage.tsx
+++ app/components/SignInPage/SignInPage.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import Image from "next/image";
 import LoginPicture from "../../../public/images/login-picture.png";
 import SignInForm from "../SignInForm/SignInForm";
 
-export default function SignInPage() {
+export default async function SignInPage() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token");
+
+  if (token) {
+    redirect("/dashboard");
+  }
+
   const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
 
   return (
-    <div className="w-[1920px]  flex mx-auto">
+    <div className="w-[1920px] flex mx-auto">
       <Image
         src={LoginPicture}
         alt="loginPic"
@@ -15,8 +23,8 @@ export default function SignInPage() {
         height={1000}
         className="w-[948px] h-[1000px]"
       />
-      <div className="w-full justify-center flex mt-[241px] mr-[36px]  ">
-        <div className="w-[554px] ">
+      <div className="w-full justify-center flex mt-[241px] mr-[36px]">
+        <div className="w-[554px]">
           <p className="text-[#10151F] font-semibold text-[42px] mb-8">
             Log In
           </p>
